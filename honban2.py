3d32d7426650c6f5d45e89cbdcae64622f064a61
Yuta <tsuchiyayuuta@YutaMacBook-Pro.local>
2025-01-27 23:04:06 +0900

    fixed ver

--- honban2.py
+++ honban2.py
@@ -48,8 +48,15 @@ if st.session_state.page == "home":
         st.session_state.agreement_check = False
         st.session_state.form_submitted = 0
 
-    hide_github_icon = """.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }"""
-    st.markdown(hide_github_icon, unsafe_allow_html=True)
+    # CSSでカスタマイズ
+    hide_streamlit_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        </style>
+    """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
     st.markdown('# ユーザアンケート トップページ')
 
